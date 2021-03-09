2f37652188f7e0c616f53968cc385d9984ff0527
Sarehon Taylor <sarehontaylor@icloud.com>
2021-03-08 21:21:48 -0500

    first changes to .js file

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-onst history = JSON.parse(localStorage.getItem('inputCity')
+//Vars
+var searchButton = $(".searchButton";)
 
-//filters history
-const uniqueHistory = [...new Set (history)
-
-//call openweathermap data for lastCity
-function buildContent(city) {
-    //get last city in history array
-// conv lastCity to cordinates
-Geolocation ='https://api.open
-
-fetch(Geolocation)
-.then(responce => responce.json())
-.then(data => {
-    console.log(data);
-})
-});
-}
-
-//search bar and button
-searchButton.click(function () {
-inputCity = searchBar.val()
-//push current inputCity to cityArray via LocalStorage
-history.pushState(inputCity);
-localStorage.setItem('inputCity', JSON.stringify(history));
-buildContent(inputCity);
-});
-
-//create search history buttons
-for (i = 0; i < uniqueHistory.length; i++) {
-    searchHistory.prepend(
-    $'<button/>'
-    .addClass('.historyBtn')
-    .text(uniqueHistory)[i])
-    );
-}
+var Key "ba8d45d4558f27276ce537d29316860b"
