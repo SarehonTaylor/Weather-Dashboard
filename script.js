ec5dc8062505da1d0aa8acb0b841c91f6dfb120b
Sarehon Taylor <sarehontaylor@icloud.com>
2021-05-22 14:57:26 -0400

    made more script changes to attempt data render on page

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let input = document.querySelector("input");
 let current = document.querySelector("#current");
 let historyCount = document.querySelector(".historycount");
 
-let button = document.querySelector("#searchcity");
-button.addEventListener("click", createHistory);
-button.addEventListener("click", getCurrent);
+let button = document.querySelector("#searchBtn");
+// button.addEventListener("click", createHistory);
+// button.addEventListener("click", getCurrent);
 
 let array = [];
 let localArray = JSON.parse(localStorage.getItem("cities"));
@@ -35,6 +35,41 @@ function renderItems(city,data){
     rendercurrentweather(city,data.current)
     renderforcast(data.daily)
 }
+
+function rendercurrentweather(city,weather,timezone){
+   let date = dayjs.unix(unixts).format('MM/DD/YYYY')
+   let tempf = weather.temp   
+   let {humidity} = weather.humidity
+    let windmph = weather.wind_speed
+    let uvi = weather.uvi
+    let card = document.createElement('div')
+    let cardbody = document.createElement('div')
+    let heading = document.createElement('h2')
+    // let weathericon = document.createElement('img')
+    let tempatureEl = document.createElement('p')
+    let windEl = document.createElement('p') 
+    let humidityEl = document.createElement('p')
+    let uvEl = document.createElement('p')
+    card.setAttribute('class','card')
+    cardbody.setAttribute('class','card-body')
+    card.append(cardbody)
+    heading.setAttribute('class','h3 card-title')
+    windEl.setAttribute('class','card-text')
+    humidityEl.setAttribute('class','card-text')
+    tempatureEl.setAttribute('class','card-text')
+
+
+
+    heading.textContent=`${city},${date}`
+    tempatureEl.textContent = `temp: ${tempf} F`
+    windEl.textContent = `wind: ${windmph}`
+    humidity.textContent = `humidity: ${humidity}`
+    todaycontainer.innerHTML = ""
+    todaycontainer.append (card)
+    
+}
+
+
 
 function checkweather () {
 let {lat} = location;
@@ -74,4 +109,46 @@ fetch(apiurl)
 .catch(function(err){
     console.error(err)
 })    
+}
+//create card
+function rendercard(){
+let unixts = forcast.dt 
+let iconurl //lookup weather icon api call
+let icondescription = forcast.weather[0].description
+let tempf = forcast.temp.day    
+let {humidity} = forcast
+let windmph = forcast.wind_speed
+
+let col = document.createElement('div')
+let card = document.createElement('div')
+let cardbody = document.createElement('div')
+let cardtitle = document.createElement('h6')
+let weathericon = document.createElement('img')
+let tempatureEl = document.createElement('p')
+let windEl = document.createElement('p') 
+let humidityEl = document.createElement('p')
+
+col.append(card)
+card.append(cardbody)
+cardbody.append(cardtitle, weathericon, tempatureEl, windEl, humidityEl)
+
+col.setAttribute('class','col-md')
+col.classList.add('five-day-card')
+card.setAttribute('class','card bg-primary h-100 text-white')
+cardbody.setAttribute('class','card-body p-2')
+cardtitle.setAttribute('class','card-title')
+weathericon.setAttribute('class','card-text')
+windEl.setAttribute('class','card-text')
+humidityEl.setAttribute('class','card-text')
+tempatureEl.setAttribute('class','card-text')
+
+cardtitle.textContent = dayjs.unix(unixts).format('MM/DD/YYYY')
+weathericon.setAttribute('src', iconurl)
+weathericon.setAttribute('alt',icondescription)
+tempatureEl.textContent = `temp: ${tempf} F`
+windEl.textContent = `wind: ${windmph}`
+humidity.textContent = `humidity: ${humidity}`
+
+forcastContainer.append(col)
+console.log()
 }
